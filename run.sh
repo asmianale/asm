6c2d136c0eb0c8bf7a78482b7adb3fe32669ceda
asmianale <161240109+asmianale@users.noreply.github.com>
2024-03-20 05:03:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-75707d68-b903-4127-a75b-e6da24a2c3d6/)
 
 
 echo "$curl_output" >> Update.tsx
